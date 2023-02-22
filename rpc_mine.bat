2a4a67ebd2e6573a8d2bc89728fbfdfccb443d80
tuanbui186 <11520451@gm.uit.edu.vn>
2023-02-22 15:01:52 +0700

    Add files via upload

---
+++ rpc_mine.bat
@@ -0,0 +1,7 @@
+:loop
+
+astrominer.exe -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjaymlc0zyx0w -r community-pools.mysrv.cloud:10300 -r1 dero-node-sg.mysrv.cloud:10300 -p rpc
+
+timeout /t 5
+
+goto loop
---
+++ stratum_mine.bat
@@ -0,0 +1,7 @@
+:loop
+
+astrominer.exe -w YOUR_ADDRESS -r YOUR_RPC_IP:YOUR_RPC_PORT -p stratum
+
+timeout /t 5
+
+goto loop
