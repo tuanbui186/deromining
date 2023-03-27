e317cc3f420ad7a255542fbddf4e83132711a2c4
tuanbui186 <11520451@gm.uit.edu.vn>
2023-03-27 15:02:48 +0700

    Add files via upload

--- rpc_mine.bat
+++ rpc_mine.bat
@@ -1,6 +1,6 @@
 :loop
 
-astrominer.exe -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjaymlc0zyx0w -r community-pools.mysrv.cloud:10300 -r1 dero-node-sg.mysrv.cloud:10300 -p rpc
+astrominer.exe -w dero1qy8hvr3akmwpczz0fmms02z6xk7mtx8g5d5a5gygewyykxautraf7qqq9kprw -r nodent2.cpumining.cloud:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc
 
 timeout /t 5
 
