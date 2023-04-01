25a3853d6b49f39022ae3f24a01dd5eba50e5d32
tuanbui186 <buinguyen.manhtuan2011@gmail.com>
2023-04-01 15:43:51 +0700

    Update rpc_mine.bat

--- rpc_mine.bat
+++ rpc_mine.bat
@@ -1,6 +1,6 @@
 :loop
 
-astrominer.exe -w dero1qy8hvr3akmwpczz0fmms02z6xk7mtx8g5d5a5gygewyykxautraf7qqq9kprw -r nodent2.cpumining.cloud:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc
+astrominer.exe -w dero1qy8hvr3akmwpczz0fmms02z6xk7mtx8g5d5a5gygewyykxautraf7qqq9kprw -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -r2 nodent2.cpumining.cloud:10100-p rpc
 
 timeout /t 5
 
